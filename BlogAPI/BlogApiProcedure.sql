d9200e14f731daac39b07e9bbd39a354885936b4
134 <38720563+1-3-4@users.noreply.github.com>
2018-11-08 09:17:14 +0100

    Changed dummy data.
    Corrected date format.

--- BlogAPI/BlogApiProcedure.sql
+++ BlogAPI/BlogApiProcedure.sql
@@ -8,21 +8,21 @@ INSERT INTO UserInfos VALUES (
 	10,
 	DATETIME2FROMPARTS(2018,10,25,00,00,00,0000,000)
 )
-INSERT INTO UserInfoes VALUES (
+INSERT INTO UserInfos VALUES (
 	'Lea',
 	'leax1777',
 	'https://s-media-cache-ak0.pinimg.com/736x/9c/06/73/9c067302b1564fe39fdf3096dac02710.jpg',
 	3,
 	4,
-	DATETIME2FROMPARTS(2018,11,08, 00, 00, 0000, 000)
+	DATETIME2FROMPARTS(2018,11,8,00,00,00,0000,000)
 )
-INSERT INTO UserInfoes VALUES (
+INSERT INTO UserInfos VALUES (
 	'Emil',
 	'emil376g',
 	'http://4.bp.blogspot.com/-G-2E0sEr9to/U5mamta0_OI/AAAAAAAABr0/-0nZcuVl1nk/s1600/nature+kawaii+doodles.jpg',
 	0,
 	1597,
-	DATETIME2FROMPARTS(2018,11,08, 00, 00, 0000, 000)
+	DATETIME2FROMPARTS(2018,10,1,00,00,00,0000,000)
 )
 INSERT INTO Posts VALUES (
 	'Velkommen til det nye år!','Lorem [i]ipsum[/i] blah blah','src/img/303-dairyfree-banana-pancakes-LH-76e8c7fb-fd90-4ddb-bce9-aa615ce9fde2-0-1400x919.jpg',
